2ca70e49bb56022bc13837bd5e48e64efe0de9fd
Neal JIn Oh <no4259@gmail.com>
2020-03-05 02:10:37 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -15,8 +15,8 @@ export default function manageTodo(state = {
  
     case 'DELETE_TODO':
  
-      return {todos: state.todos.filter(todo => todo !== action.payload)}
- 
+  return {todos: state.todos.filter(todo => todo.id !== action.payload)}
+  
     default:
       return state;
   }
